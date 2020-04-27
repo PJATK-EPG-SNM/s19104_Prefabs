1d8bc7517103ff6cb7d7b194c44ff1952d0d299e
s19104 <61684594+s19104@users.noreply.github.com>
2020-04-27 11:15:42 +0200

    01

---
+++ Prefabs/Assets/Scripts/Canon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    public float period = 1;
+    private float lastBallTime;
+
+    public Canonball prefab;
+    public Transform startTransform;
+
+    public float rotationSpeed = 50;
+    private float direction = -1;
+    
+    void Start()
+    {
+        lastBallTime = 0;
+    }
+
+    
+    void Update()
+    {
+        lastBallTime += Time.deltaTime;
+
+        if(lastBallTime > period)
+        {
+            lastBallTime = 0;
+            Instantiate(prefab, startTransform.position, startTransform.rotation);
+        }
+
+        if (lastBallTime > period/2)
+        {
+            float rotationValue = rotationSpeed * direction * Time.deltaTime;
+            Vector3 rotateVector = new Vector3(0, 0, rotationValue);
+            startTransform.Rotate(rotateVector);
+
+            if(startTransform.rotation.z < -0.999 || startTransform.rotation.z > 0)
+            {
+                direction *= -1;
+            }
+        }
+    }
+
+}
